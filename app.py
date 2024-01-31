680acb7c3b1fc83e36dd74bc88649f4f640d77fa
Thiago Flausino <126170486+Flausinotc@users.noreply.github.com>
2024-01-31 08:41:56 -0300

    Update app.py
    Atualização com algumas melhorias

--- app.py
+++ app.py
@@ -2,38 +2,71 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import pandas as pd
 from time import sleep
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+import os
+import re
 
-# Extrair descrições de uma página com base no termo de pesquisa
-def extrair_descricoes(termo_pesquisa):
+driver = webdriver.Chrome()
+
+def extrair_descricoes_e_enviar_curriculo(termo_pesquisa):
     driver.get(f'https://www.classitudodiariodaregiao.com.br/empregos&s={termo_pesquisa}')
     sleep(3)
 
-    # Localiza todos os elementos que contêm as descrições de vagas
     elementos = driver.find_elements(By.CLASS_NAME, 'descricao')
 
-    # Cria uma lista para armazenar as descrições
     descricoes = []
+    emails = []
 
-    # Itere pelos elementos e extraia as descrições
     for elemento in elementos:
         texto = elemento.text
         descricoes.append(texto)
 
-    # Cria um DataFrame com as descrições
-    df = pd.DataFrame({'Descrição da Vaga': descricoes})
+        matches = re.findall(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b', texto)
 
-    # Exporta o DataFrame para o Excel
+        if matches:
+            emails.extend(matches)
+        else:
+            continue
+
+    df = pd.DataFrame({'Descrição da Vaga': descricoes, 'E-mail': emails})
     df.to_excel(f'dados_vagas_{termo_pesquisa}.xlsx', index=False)
 
-# Inicialize o driver do Chrome
-driver = webdriver.Chrome()
 
-# Termos de pesquisa
-termos_pesquisa = ['estagiario', 'admin', 'vendedor']
+    enviar_curriculo_para_emails(emails)
 
-# Extrair descrições para cada termo de pesquisa
+def enviar_curriculo_para_emails(emails):
+    remetente_email = 'no-reply@gmail.com'
+    senha = 'aaah rá achou que eu ia deixar a senha né?'
+    assunto = 'Currículo para a vaga'
+
+    mensagem = MIMEMultipart()
+    mensagem['From'] = remetente_email
+    mensagem['Subject'] = assunto
+
+    corpo_mensagem = 'Segue curriculo para a vaga anunciada no diario web.'
+    mensagem.attach(MIMEText(corpo_mensagem, 'plain'))
+
+    curriculo_path = 'C:/Users/thiago.cardoso/Documents/Matheus.python/esseaqui.jpg'
+    curriculo_anexo = MIMEApplication(open(curriculo_path, 'rb').read())
+    curriculo_anexo.add_header('Content-Disposition', 'attachment', filename=os.path.basename(curriculo_path))
+    mensagem.attach(curriculo_anexo)
+
+    servidor_smtp = smtplib.SMTP('Servidor gato', 587)
+    servidor_smtp.starttls()
+    servidor_smtp.login(remetente_email, senha)
+
+    for destinatario_email in emails:
+        mensagem['To'] = destinatario_email
+        servidor_smtp.sendmail(remetente_email, destinatario_email, mensagem.as_string())
+
+    servidor_smtp.quit()
+
+termos_pesquisa = ['estagiario', 'programador', 'vendedor']
+
 for termo in termos_pesquisa:
-    extrair_descricoes(termo)
+    extrair_descricoes_e_enviar_curriculo(termo)
 
-# Feche o driver do Chrome quando terminar
 driver.quit()
